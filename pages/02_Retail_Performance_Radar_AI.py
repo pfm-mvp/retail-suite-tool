396600e1a273b86251b7858d56c65e6337396644
pfm-mvp <christiaan.vanrooijen@pfm-intelligence.com>
2025-08-17 20:55:23 +0200

    Update 02_Retail_Performance_Radar_AI.py

--- pages/02_Retail_Performance_Radar_AI.py
+++ pages/02_Retail_Performance_Radar_AI.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 
 # ---------- Imports / mapping ----------
 sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../'))
-from shop_mapping import SHOP_NAME_MAP
+from helpers_shop import ID_TO_NAME, NAME_TO_ID, REGIONS, get_ids_by_region, get_name_by_id, get_region_by_id
 from helpers_normalize import normalize_vemcount_response
 
 st.set_page_config(page_title="Region Performance Radar", page_icon="🧭", layout="wide")
